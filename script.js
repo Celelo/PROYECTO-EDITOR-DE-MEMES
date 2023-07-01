90328c4f76f41c8f321308cdf005774245520fcf
Célelo <mariaceleste.lopez41@gmail.com>
2023-07-01 00:36:24 -0300

    incorporo document de los inputs al js

---
+++ script.js
@@ -0,0 +1,51 @@
+//INICIO DE PANELES
+
+//INICIO PANEL IMAGEN 
+const inputUrl = document.getElementById('texturl')
+
+const inputColorBackgroud = document.getElementById('colorbackgroundimg')
+
+// INICIO FILTROS 
+const inputGlow = document.getElementById('rangeglow')
+const imputOpacity = document.getElementById('rangeopacity')
+const inputContrast = document.getElementById('rangecontrast')
+const inputBlur = document.getElementById('rangeblur')
+const inputRangeGrayScale = document.getElementById('rangegrayscale')
+const inputSepia = document.getElementById('rangesepia')
+const inputHue = document.getElementById('rangehue')
+const inputCrowded = document.getElementById('rangecrowded')
+const inputNegative  = document.getElementById('rangenegative')
+
+//FIN PANEL IMAGEN 
+
+//INICIO PANEL TEXTO
+
+//texto superior
+const inputOffTopText = document.getElementById('offtoptext')
+
+
+//texto inferior 
+const inputOffBottomText = document.getElementById('offbottomtext')
+
+
+//tamaño de fuente 
+const inputFontSize = document.getElementById('fontsize')
+
+
+//color texto
+const inputTextColor = document.getElementById('textcolor')
+
+//color fondo 
+const inputColorBackgroudText = document.getElementById('colorbackgroundtext')
+
+
+//cheked fondo transparente 
+const inputTransparentBackground = document.getElementById('transparentbackground')
+
+
+//espaciado
+const inputSpacing = document.getElementById('spacingtext')
+
+//FIN PANEL TEXTO
+
+//FIN DE PANELES
