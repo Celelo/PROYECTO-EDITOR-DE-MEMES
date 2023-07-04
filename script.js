ef2a21a70e4619ff21eceb61762469180276689b
Célelo <mariaceleste.lopez41@gmail.com>
2023-07-04 00:17:33 -0300

    modo panel con botones hechos , mitad de modo oscuro hecho

--- script.js
+++ script.js
@@ -1,7 +1,80 @@
+
+//inicio cambio de panel imagen y texto por boton 
+
+ // boton img y texto
+const buttonImgMenu = document.getElementById('button-img-menu');
+
+const buttonTextMenu = document.getElementById('button-text-menu');
+
+//sections img y text
+const sectionImg = document.getElementById('panel-img');
+
+const sectionText = document.getElementById('panel-text');
+
+
+//event handler de img y text 
+const hideTextSection = () => {
+    sectionText.classList.remove('hidden')
+    sectionImg.classList.add('hidden')
+}
+
+
+const hideImgSection = () => {
+    sectionImg.classList.remove('hidden')
+    sectionText.classList.add('hidden')
+}
+
+
+// event listener de img y text
+buttonImgMenu.addEventListener('click', () => hideImgSection())
+
+buttonTextMenu.addEventListener('click', () => hideTextSection())
+
+//fin cambio de panel imagen y texto por boton 
+
+
 //INICIO DE PANELES
 
 //INICIO PANEL IMAGEN 
-const inputUrl = document.getElementById('texturl')
+
+//inicio de imagen meme e input URL
+const inputUrl = document.getElementById('meme-url')
+const imgMeme = document.getElementById('meme-img')
+console.log(imgMeme)
+console.log(inputUrl)
+
+const changeBackground = () => {
+    imgMeme.style.backgroundImage = `url('${inputUrl.value}')`
+}
+inputUrl.addEventListener('input', () => changeBackground())
+
+// fin de imagen meme e input URL
+
+// inicio de modo oscuro/claro
+
+//header
+const modHeader = document.getElementById('header-mods-colors')
+const modBody = document.getElementById('body-mods-colors')
+const botonWhite = document.getElementById('button-white')
+const botonBlack = document.getElementById('button-black')
+
+const modBlack = () => {
+    modHeader.classList.remove('header-white')
+    modHeader.classList.add('header-black')
+    modBody.classList.remove('body-white')
+    modBody.classList.add('body-black')
+    sectionImg.classList.remove('aside-white')
+    sectionText.classList.remove('aside-white')
+    sectionImg.classList.add('aside-black')
+    sectionText.classList.add('aside-black')
+}
+
+
+
+botonBlack.addEventListener('click', () => modBlack())
+
+// fin de modo oscuro/claro
+
 
 const inputColorBackgroud = document.getElementById('colorbackgroundimg')
 
